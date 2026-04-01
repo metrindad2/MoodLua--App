232fd8edbdc0d3fb8c674ecf8332af4cd6a17c3a
Maria Eduarda Trindade <mariaeduardatrindad2@gmail.com>
2026-04-01 01:31:06 +0000

    o calendario nao pode ter um prazo para acabar  ele tem q ser de 2026 se

--- src/components/period-registration-modal.tsx
+++ src/components/period-registration-modal.tsx
@@ -158,7 +158,6 @@ export function PeriodRegistrationModal({
                     initialDate={month}
                     selectedDates={selectedDays}
                     onDateClick={handleDayClick}
-                    disableFutureDates
                     previsionRange={previsionRange}
                   />
                 </div>
